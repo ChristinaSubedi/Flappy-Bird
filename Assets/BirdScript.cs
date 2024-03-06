050a5177dc1d92350c1a2d7cef326b04aa40a06c
Christina Subedi <96991580+ChristinaSubedi@users.noreply.github.com>
2024-03-06 17:37:43 +0100

    updated speed in script of pipespawner

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -4,50 +4,47 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
-	//Reference the body of bird
-	public Rigidbody2D myRigidbody;
-	public float flapStrength;	
+    // Reference the body of bird
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;    
     public LogicScript logic;
-    public bool birdIsAlive= true;
-    //if goes beneath the screen dead
+    public bool birdIsAlive = true;
+    // If goes beneath the screen, dead
     public float minYPosition = -16.5f;
 
     
     // Start is called before the first frame update
     void Start()
     {
-        //Code as soon as it starts
+        // Code as soon as it starts
         gameObject.name = "Bob";
-        //access the logic script
-        logic=GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
-
-        
+        // Access the logic script
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Fires every line of code over and over in each frame
-        //Checks if space bar is clicked
-        if  (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
+        // Fires every line of code over and over in each frame
+        // Checks if space bar is clicked
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
-        //Vector2.up sets velocity to (0,1) so only in y dir
-        	myRigidbody.velocity = Vector2.up * flapStrength ; 
-	}    
+            // Vector2.up sets velocity to (0,1) so only in y dir
+            myRigidbody.velocity = Vector2.up * flapStrength; 
+        }    
 
-            // Check if the bird goes below the minimum Y position
+        // Check if the bird goes below the minimum Y position
         if (transform.position.y < minYPosition && birdIsAlive)
         {
             // Trigger game over
             logic.gameOver();
             birdIsAlive = false;
         }
-
-       
     }
 
-    public void OnCollisionEnter2D(Collision2D collision){//Not trigger objects
+    public void OnCollisionEnter2D(Collision2D collision)
+    { // Not trigger objects
         logic.gameOver();
-        birdIsAlive=false;
+        birdIsAlive = false;
     }
 }
--- Assets/PipeSpawner.cs
+++ Assets/PipeSpawner.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PipeSpawner : MonoBehaviour
 {
-	public GameObject pipe;
-	public float spawnRate = 3; //how many seconds before pipe spawns
-	private float timer = 0; //counts number of seconds
-	public float heightOffset =10; //dist of pipes
+    public GameObject pipe;
+    public float spawnRate = 3; // How many seconds before pipe spawns
+    private float timer = 0; // Counts number of seconds
+    public float heightOffset = 10; // Distance of pipes
+    public float pipeSpeed = 3f; // Speed at which pipes move to the left
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,20 +19,27 @@ public class PipeSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    	if (timer < spawnRate)
-    	{
-    		timer += Time.deltaTime;
-    	} else
-    	{
-    		timer =0;
-        	spawnPipe();
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
         }
-        
+        else
+        {
+            timer = 0;
+            spawnPipe();
+        }
+
+        // Move existing pipes to the left
+        foreach (Transform child in transform)
+        {
+            child.Translate(Vector3.left * pipeSpeed * Time.deltaTime);
+        }
     }
-    
-    void spawnPipe(){
-    	float lowestPoint = transform.position.y - heightOffset;
-    	float highestPoint = transform.position.y + heightOffset;
-        Instantiate(pipe, new Vector3 (transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+
+    void spawnPipe()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation, transform);
     }
 }
